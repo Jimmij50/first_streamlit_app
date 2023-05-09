88d30ac061e32d458431f228553d4d04c759800f
Ziyu Li <jimmij50@163.com>
2023-05-09 09:40:34 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,7 @@ streamlit.title("hello world")
 
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("pick some fruits:" ,list(my_fruit_list.index))
+fruit_selected = streamlit.multiselect("pick some fruits:" ,list(my_fruit_list.index),['Avocado'])
+fruits_to_show=my_fruit_list.loc[fruit_selected]
+
 streamlit.dataframe(my_fruit_list)
